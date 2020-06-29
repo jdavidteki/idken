6dd2474a50d41f0cb38e13483248e83fb7a9c540
= <jdavid@yext.com>
2020-06-29 00:07:56 -0400

    Updates

---
+++ precache-manifest.968d96c553663b787c718ad10dfaa5c2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e18986ce4874b29a8cb",
+    "url": "/idken/static/js/app.7ee529c3.chunk.js"
+  },
+  {
+    "revision": "a093ab8facc7537ec62e",
+    "url": "/idken/static/js/runtime~app.b4a2df9d.js"
+  },
+  {
+    "revision": "79e71e3efc752c7d04d4",
+    "url": "/idken/static/js/2.ff56be39.chunk.js"
+  },
+  {
+    "revision": "e0e01978abbeed2932f7ddc276e17b53",
+    "url": "/idken/static/media/logo.e0e01978.png"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/idken/expo-service-worker.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/idken/serve.json"
+  },
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/idken/favicon-16.png"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/idken/favicon-32.png"
+  },
+  {
+    "revision": "5e52768b0f7cbb62cbe07751add33101",
+    "url": "/idken/favicon.ico"
+  },
+  {
+    "revision": "48f36d9be0e0e7997f3bef43765b8a7f",
+    "url": "/idken/index.html"
+  },
+  {
+    "revision": "ad4fc6e3fd0335c05ab220940a4947d9",
+    "url": "/idken/manifest.json"
+  },
+  {
+    "revision": "d3c6cd4371ba11f753b87a7d7f285009",
+    "url": "/idken/register-service-worker.js"
+  }
+];
