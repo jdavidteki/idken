0ca3f47a0213aead42f12b4b7e75aefe07a8d4f6
= <jdavid@yext.com>
2020-07-09 01:39:01 -0400

    build

--- web-build/register-service-worker.js
+++ web-build/register-service-worker.js
@@ -3,7 +3,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
     navigator.serviceWorker
-      .register('/expo-service-worker.js', { scope: '/' })
+      .register('/idken/expo-service-worker.js', { scope: '/idken/' })
       .then(function (info) {
         // console.info('Registered service-worker', info);
       })
